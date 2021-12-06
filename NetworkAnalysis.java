daee17995b50f0a74879ac1d438a82650f21f204
Osama Muhammad <osamamohammed3623@gmail.com>
2021-12-06 18:46:28 +0200

    finished most influencer user analysis

--- NetworkAnalysis.java
+++ NetworkAnalysis.java
@@ -4,6 +4,7 @@ public class NetworkAnalysis {
 	
 	ArrayList<String> rows;
 
+
 	NetworkAnalysis(String xml){
 		rows = commonMethods.xmlToRows(xml);
 	}
@@ -18,9 +19,11 @@ public class NetworkAnalysis {
 		int currentUserFollowers=0, currentUserID=0;
 
 		for (int i=0; i< rows.size(); i++){
-			
+			/**
+			 * user tag, followed by id tag, so the following row contains ID number
+			 */
 			if (rows.get(i).equals("<user>") && rows.get(i+1).equals("<id>")){
-				currentUserID = getUserID(i+1);
+				currentUserID = getUserID(i+2);
 				currentUserFollowers = getCurrentUserFollowers(i);
 
 				if (currentUserFollowers > mostInfluencerFollowers){
@@ -29,7 +32,7 @@ public class NetworkAnalysis {
 					mostInfluencerFollowers = currentUserFollowers;
 				}
 			}
-	}
+		}
 
 		return mostInfluencerID;
 	}
@@ -39,16 +42,30 @@ public class NetworkAnalysis {
 	 * Desc: retruns number of followers for a specific user 
 	 * */
 	public int getCurrentUserFollowers(int startingIndex) {
-		return 0;
+
+		int followers =0;
+
+		/** count how many <follower> tags are there, till we get to </followers> closing tag */
+		for (int i=startingIndex; i< rows.size(); i++) {
+			
+			if (rows.get(i).equals("<follower>")){
+				followers++;
+			}else if (rows.get(i).equals("</followers>")){
+				break;
+			}
+		}
+
+		return followers;
 	}
 
 
 	/**
-	 * Desc: 
+	 * Desc: returns the ID of a user
+	 * 	parameter: index of the row that holds the ID number
 	 */
-	public int getUserID(int startingIndex){
+	public int getUserID(int Index){
 		int id=0;
-		/* cast string to int */
+		id = Integer.parseInt(rows.get(Index));
 		return id;
 	}
 }
